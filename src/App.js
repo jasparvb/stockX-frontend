5736becbff9c356ba8bf3a5c490fce2ac3628cf8
von Buelow <jaspar@UMICH.EDU>
2020-09-04 17:07:50 -0400

    index and app setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import './App.css';
+import NavHeader from './NavHeader';
+import Routes from './Routes';
 
 function App() {
+  
   return (
     <div className="App">
+      <NavHeader />
+      <Routes />
     </div>
   );
 }
