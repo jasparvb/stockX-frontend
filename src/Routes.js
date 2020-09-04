d8bda78aca93681ce0523dd34bbbae93b92472b4
von Buelow <jaspar@UMICH.EDU>
2020-09-04 17:30:30 -0400

    Routes

---
+++ src/Routes.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Switch, Redirect, Route } from "react-router-dom";
+import Home from "./Home";
+import Login from "./Login";
+import Lists from "./Lists";
+import StockDetails from "./StockDetails";
+
+function Routes() {
+    return (
+        <Switch>
+            <Route path="/" exact>
+                <Home />
+            </Route>
+            <Route exact path="/login">
+                <Login setToken={setToken} />
+            </Route>
+            <Route path="/lists" exact>
+                <Lists />
+            </Route>
+            <Route path="/:stockId" exact>
+                <StockDetails />
+            </Route>
+            <Redirect to="/" />
+        </Switch>
+    );
+}
+
+export default Routes;
