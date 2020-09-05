42d5d89101e3bed54da9fa15912f147c329847b0
von Buelow <jaspar@UMICH.EDU>
2020-09-05 16:09:48 -0400

    favicon

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
+import stockXLogo from "./stockX-logo.png";
+import './NavBar.css';
 
 function Navbar() {
     return (
-        <nav className="navbar navbar-light bg-info">
+        <nav className="navbar navbar-light">
         <Link to="/" className="navbar-brand text-light">
-            <img src="%PUBLIC_URL%/stockX-logo.png"/>
+            <img src={stockXLogo} />
         </Link>
         <ul className="navbar-nav flex-row">
             <li className="nav-item">
