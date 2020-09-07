5ec1857e61d20029f6a8b47752b27c42a15bc7b9
von Buelow <jaspar@UMICH.EDU>
2020-09-07 11:48:03 -0400

    Handle submit and click away

--- src/AutoCompleteItem.js
+++ src/AutoCompleteItem.js
@@ -1,15 +1,16 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import './AutoCompleteItem.css';
 
-function AutoCompleteItem({id, ticker, name, assetType, countryCode, clearForm, selected, setSelected}) {
+function AutoCompleteItem({id, ticker, name, assetType, countryCode, handleSubmit, selected, setSelected}) {
     return (
-      <Link to={`/detail/${ticker}`} onClick={clearForm} onMouseEnter={() => setSelected(id)}>
-        <div className={`AutoCompleteItem ${selected === id ? "active" : ""}`}>
-          <h3>{ticker} &ndash; {name}</h3>
-          <p>{countryCode} | {assetType}</p>
-        </div>
-      </Link>
+      <div 
+        className={`AutoCompleteItem ${selected - 1 === id ? "active" : ""}`}
+        onClick={handleSubmit} 
+        onMouseEnter={() => setSelected(id + 1)}
+      >
+        <h3>{ticker} &ndash; {name}</h3>
+        <p>{countryCode} | {assetType}</p>
+      </div>
     );
 }
 
