5e63f5d4ce3b51ad045f3cdc01b5f9d8dfbfdcd5
von Buelow <jaspar@UMICH.EDU>
2020-09-11 22:11:30 -0400

    AddStockForm component

--- src/AddStockForm.js
+++ src/AddStockForm.js
@@ -1,10 +1,12 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from 'react-redux';
 import { addAlert } from './actions/alerts';
 import { addNewStockAPI } from './actions/lists';
 import './AddStockForm.css';
+import { useClickOutside } from 'react-click-outside-hook';
+import AddListForm from './AddListForm';
 
-function AddStockForm({ticker, name}) {
+function AddStockForm({ticker, name, setFormVisible}) {
   const dispatch = useDispatch();
 
   const INITIAL_STATE = { 
@@ -12,6 +14,7 @@ function AddStockForm({ticker, name}) {
   };
 
   const [text, setText] = useState(INITIAL_STATE);
+  const [ref, hasClickedOutside] = useClickOutside();
 
   function handleSubmit(evt) {
     evt.preventDefault();
@@ -26,12 +29,20 @@ function AddStockForm({ticker, name}) {
     setText({title: value});
   };
 
+  useEffect(() => {
+    if(hasClickedOutside) {
+      setFormVisible(false);
+    }
+  },[hasClickedOutside]);
+
   return (
-    <div className="AddStockForm container-fluid text-left">
+    <div className="AddStockForm container-fluid text-left" onClick={hideForm} >
+      <div className="overlay"></div>
       <div className="form-container">
-        <div className="form-column">
-          <h3>Enter a title to create a new list</h3>
-          <p>Once you create a list, you can search for stocks and add them to your list for easy tracking.</p>
+        <div ref={ref} className="form-column">
+          <h3>Create a new list</h3>
+          <AddListForm />
+          <p>Add stocks to your lists below</p>
           <form onSubmit={handleSubmit}>
             <div className="form-group">
               <input 
