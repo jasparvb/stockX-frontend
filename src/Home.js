1d1ce4b6dfc77be57afb95a8bcdfc1cd10be47cc
von Buelow <jaspar@UMICH.EDU>
2020-09-17 22:25:47 -0400

    Lowercase name

--- src/Home.js
+++ src/Home.js
@@ -10,7 +10,7 @@ function Home() {
     <section className="Home col-md-12 p-0">
       <div className="banner container-fluid text-center text-light m-0 p-5" style={{ backgroundImage: `url(${BannerBg})`}} >
         <div className="content-wrapper mb-5">
-          <h1 className="mb-3 font-weight-bold">StockX</h1>
+          <h1 className="mb-3 font-weight-bold">stockX</h1>
           <p className="lead">Start tracking your financial data today</p>
           <Link to="/login" className="btn btn-primary btn-lg px-5 py-3 my-5">Log In or Register</Link>
         </div>
