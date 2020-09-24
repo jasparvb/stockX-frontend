a58ea2bb90122c3f253ad2c10e943d0469c3ce9b
von Buelow <jaspar@UMICH.EDU>
2020-09-24 18:21:31 -0400

    When price n/a show error message

--- src/ListItem.js
+++ src/ListItem.js
@@ -12,8 +12,13 @@ function ListItem({id, ticker, name, quote, listId, removeStock}) {
       <p>{name}</p>
       </Link>
       <div className="text-right col-md-5">
-        <h4 className="mb-0">${quote.close.toFixed(2)}</h4>
-        <p className={`percent-change ${quote.change < 0 && "down"}`}>{quote.change >= 0 && "+"}{(quote.changePercent * 100).toFixed(2)}%</p>
+        {quote.close && quote.change && quote.changePercent ?
+        <>
+          <h4 className="mb-0">${quote.close.toFixed(2)}</h4>
+          <p className={`percent-change ${quote.change < 0 && "down"}`}>{quote.change >= 0 && "+"}{(quote.changePercent * 100).toFixed(2)}%</p>
+        </>
+        : <h4>Price currently unavailable</h4>
+        }
       </div>
       <button className="btn remove" onClick={() => removeStock(id, listId)}>
         <i className="fas fa-times" />
